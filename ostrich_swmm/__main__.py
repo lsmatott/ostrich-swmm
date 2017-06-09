f2afbb39dbee43127bf4043a8437115fafbd1497
Tom Yearke <tyearke@buffalo.edu>
2017-06-09 09:46:11 -0400

    Reduced duplicate code in main command functions

--- ostrich_swmm/__main__.py
+++ ostrich_swmm/__main__.py
@@ -66,43 +66,35 @@ def load_config_with_args(args):
     return config
 
 
-def extract_cmd(args):
+def extract_cmd(config):
     """Extract data from a SWMM binary output file.
 
     Args:
-        args (dict): Command-line arguments to use.
+        config (dict): The configuration to use.
 
     Returns:
         int: An exit code for the script.
 
     Raises:
-        ConfigException: The configuration could not be validated.
-        IOError: The config file was not found or was not readable.
-        UsageException: The configuration file was not found.
-        ValueError: The config file was not valid JSON.
+        ConfigException: The configuration was invalid.
     """
-    config = load_config_with_args(args)
     extract.perform_extraction_steps(config)
 
     return 0
 
 
-def inject_cmd(args):
+def inject_cmd(config):
     """Inject OSTRICH parameters into SWMM input before a run.
 
     Args:
-        args (dict): Command-line arguments to use.
+        config (dict): The configuration to use.
 
     Returns:
         int: An exit code for the script.
 
     Raises:
-        ConfigException: The configuration could not be validated.
-        IOError: The config file was not found or was not readable.
-        UsageException: The configuration file was not found.
-        ValueError: The config file was not valid JSON.
+        ConfigException: The configuration was invalid.
     """
-    config = load_config_with_args(args)
     inject.perform_injection(config)
 
     return 0
@@ -217,13 +209,14 @@ def main(argv=None):
 
         # Parse arguments.
         args = vars(parser.parse_args(argv[1:]))
+        config = load_config_with_args(args)
 
         # Call selected subcommand.
         subcommands = {
             'extract': extract_cmd,
             'inject': inject_cmd,
         }
-        return subcommands[args['subcommand']](args)
+        return subcommands[args['subcommand']](config)
     except (UsageException, cfg.ConfigException) as e:
         print(e.msg, file=sys.stderr)
         print('For help, use --help or [sub-command] --help.', file=sys.stderr)
