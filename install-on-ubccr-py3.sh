37647750e02681242bd65e37ffd0d9ea0e04a4fa
alikheir <alikheiri855@gmail.com>
2024-10-21 17:08:51 -0400

    fixed UB CCR Installation file

--- install-on-ubccr-py3.sh
+++ install-on-ubccr-py3.sh
@@ -7,9 +7,9 @@ fi
 
 PREFIX=$1
 
-module load anaconda-python/3.8.3
+module load anaconda-python/3.8
 
-conda create --prefix $PREFIX python=3.8.3 || exit
+conda create --prefix $PREFIX python=3.8 || exit
 
 conda config --set env_prompt '({name})' || exit
 
@@ -18,7 +18,7 @@ source activate $PREFIX || exit
 pip install jsonschema==2.6.0 || exit
 pip install numpy || exit
 pip install swmmtoolbox==4.0.14 || exit
-pip install Shapely==1.5 || exit
+pip install Shapely==1.8.5 || exit
 
 cp requirements-py3.txt requirements.txt || exit
 cp requirements-dev-py3.txt requirements-dev.txt || exit
